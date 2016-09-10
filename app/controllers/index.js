4f27d6b350aa963da9a31efd1f9dfe20f67217c4
Hernan Y.Ke <yuriqiao@gmail.com>
2016-09-10 11:40:37 -0400

    alert message and action

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -8,6 +8,15 @@ export default Ember.Controller.extend({
         //isDisabled: Ember.computed.empty('emailAddress'),
 isValid: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
   isDisabled: Ember.computed.not('isValid'),
+
+   actions: {
+
+    saveInvitation() {
+      alert(`Saving in progress: ${this.get('emailAddress')}`);
+      this.set('responseMessage', `Thank you! ${this.get('emailAddress')}`);
+      this.set('emailAddress', '');
+    }
+  },
   actualEmailAddress: Ember.computed('emailAddress', function() {
     console.log('actualEmailAddress function is called: ', this.get('emailAddress'));
   }),
